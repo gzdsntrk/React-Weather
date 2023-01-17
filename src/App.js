94e4b7f860b7e0bb101246119e7f36233f17c084
gzdsntrk <gozdesennutrkk@gmail.com>
2023-01-17 21:33:03 +0300

    master /  tailwind düzenlemesi yapıldı

--- src/App.js
+++ src/App.js
@@ -2,42 +2,42 @@ import axios from 'axios';
 import { useEffect, useState } from 'react';
 import City from './City';
 
-import './App.css';
-
 function App() {
 
-  const [search, setSearch] = useState("");
+  const [search, setSearch] = useState("istanbul");
   const [city, setCity] = useState([]);
 
-  const getApi = async () => {
-    const key = '7d78c82a12c8fca2c3762748f39530d1';
-    try {
-      const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=${key}&units=metric`);
-      console.log(response);
-      setCity(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-
   useEffect(() => {
+    const getApi = async () => {
+      const key = '7d78c82a12c8fca2c3762748f39530d1';
+      try {
+        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${search}&APPID=${key}&units=metric`);
+        setCity(response.data);
+      } catch (error) {
+        console.error(error);
+      }
+    }
     getApi();
   }, [search])
 
   return (
-    <div className="App">
-      <>
-        <input
-          onChange={(e) => setSearch(e.target.value)}
-          type="text" placeholder="Placeholder"
-          className=" border-8 px-3 w-[250px] py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring" />
+    <>
+      <div className="weather-box">
+        <div className="weather-search">
+          <input
+            onChange={(e) => setSearch(e.target.value)}
+            type="text" placeholder="Find city"
+            className="border-8 px-3 w-[250px] py-3 placeholder-blueGray-300 text-blueGray-600 relative bg-white  rounded text-sm border-0 shadow outline-none focus:outline-none focus:ring" />
+
+        </div>
         {city.id && (
           <City city={city} />
         )
 
         }
-      </>
-    </div>
+
+      </div>
+    </>
   );
 }
 
--- src/City.jsx
+++ src/City.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const City = ({city}) => {
   return (
     <div>
-        <div>
-            <h1>{city?.main?.temp}</h1>
-            <h1>{city?.name}</h1>
-            <h1>{city?.weather[0]?.main}</h1>
+        <div className='weather-status'>
+            <span>City: {city?.name}</span>
+            <span>Temperature: {city?.main?.temp}</span>
+            <span>Status: {city?.weather[0]?.main}</span>
         </div>
     </div>
   )
